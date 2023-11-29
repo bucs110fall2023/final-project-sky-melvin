ca5f0b2f3133163804df7ce73e7569c62e01af2a
Melvin Zhou <mzhou5@binghamton.edu>
2023-11-29 17:37:54 -0500

    GUI

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -12,7 +12,60 @@ class Controller:
       for event in pygame.event.get():
         if event.type == pygame.QUIT:
           running = False
-          
+        if event.type == pygame.KEYDOWN:
+          if event.key == pygame.K_a:
+            print('a')
+          if event.key == pygame.K_b:
+            print('b') 
+          if event.key == pygame.K_c:
+            print('c') 
+          if event.key == pygame.K_d:
+            print('d') 
+          if event.key == pygame.K_e:
+            print('e') 
+          if event.key == pygame.K_f:
+            print('f')
+          if event.key == pygame.K_g:
+            print('g') 
+          if event.key == pygame.K_h:
+            print('h') 
+          if event.key == pygame.K_i:
+            print('i') 
+          if event.key == pygame.K_j:
+            print('j') 
+          if event.key == pygame.K_k:
+            print('k') 
+          if event.key == pygame.K_l:
+            print('l') 
+          if event.key == pygame.K_m:
+            print('m') 
+          if event.key == pygame.K_n:
+            print('n') 
+          if event.key == pygame.K_o:
+            print('o') 
+          if event.key == pygame.K_p:
+            print('p') 
+          if event.key == pygame.K_q:
+            print('q') 
+          if event.key == pygame.K_r:
+            print('r') 
+          if event.key == pygame.K_s:
+            print('s') 
+          if event.key == pygame.K_t:
+            print('t') 
+          if event.key == pygame.K_u:
+            print('u') 
+          if event.key == pygame.K_v:
+            print('v') 
+          if event.key == pygame.K_w:
+            print('w') 
+          if event.key == pygame.K_x:
+            print('x')
+          if event.key == pygame.K_y:
+            print('y') 
+          if event.key == pygame.K_z:
+            print('z')
+             
       screen.fill("grey")
       
       x = 50
