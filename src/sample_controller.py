ffde1dfb26b249ae1e336fbb96cb13f37fefeed3
Melvin Zhou <mzhou5@binghamton.edu>
2023-11-30 21:58:38 -0500

    GUI

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -39,192 +39,116 @@ class Controller:
                 pygame.draw.rect(screen, "black", [coord[pointer][0],coord[pointer][1], 45, 45])
               pygame.draw.rect(screen, "black", [coord[pointer][0],coord[pointer][1], 45, 45])
               print('BACK')
-          if pointer<36:
+          if pointer==36:
+            pass
+          else:
             if event.key == pygame.K_a:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('A', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('A', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_b:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('B', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('B', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_c:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('C', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('C', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_d:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('D', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('D', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_e:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('E', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('E', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_f:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('F', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('F', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_g:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('G', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('G', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_h:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('H', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('H', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_i:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('I', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('I', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_j:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('J', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('J', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_k:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('K', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('K', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_l:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('L', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('L', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_m:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('M', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('M', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_n:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('N', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('N', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_o:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('O', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('O', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_p:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('P', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('P', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_q:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('Q', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('Q', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_r:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('R', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('R', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_s:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('S', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('S', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_t:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('T', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('T', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_u:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('U', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('U', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_v:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('V', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('V', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_w:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('W', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('W', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_x:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('X', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('X', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
             if event.key == pygame.K_y:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('Y', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1 
+              text_surface = font.render('Y', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1 
             if event.key == pygame.K_z:
-              if pointer==36:
-                pass
-              else:
-                text_surface = font.render('Z', False, 'white')
-                screen.blit(text_surface, coord[pointer])
-                pointer+=1
+              text_surface = font.render('Z', False, 'white')
+              screen.blit(text_surface, coord[pointer])
+              pointer+=1
       
       pygame.display.update()
- 
+     
       clock.tick(60)  
 
     pygame.quit()
