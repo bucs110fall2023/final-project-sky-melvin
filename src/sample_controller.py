b969df25e1b8ace91cc5f52d848ab7a40b022d75
Sky Jiang <sjiang81@binghamton.edu>
2023-12-01 16:57:30 -0500

    removed the check.py and moved the stuff over to sample_controller.py. Then I added to each letter's keyboard command to add said letter to the guessed word list which then would get it compared. Just missing the code to display the result of the comparisons

--- src/sample_controller.py
+++ src/sample_controller.py
@@ -1,5 +1,7 @@
-import check
 import pygame
+import random
+import dictionary
+
 pygame.init()
 class Controller:
   
@@ -25,7 +27,8 @@ class Controller:
     end_limit=6
     front_limit=0
     count=0 # amount of tries
-    
+    wordle=Controller.wordle()
+    guess=[]
     
     while running:
       print(pointer)
@@ -40,6 +43,9 @@ class Controller:
               front_limit=end_limit #store previous end limit as front limit before increasing end_limit
               end_limit+=6
               count+=1
+              Controller.check_pos(wordle, guess)
+              ##INSERT CODE TO USE THE COLOR AND LETTER OUTPUT TO DISPLAY ONTO PYGAME
+              guess.clear()
           if event.key == pygame.K_BACKSPACE:
               if pointer>front_limit:
                 pointer-=1
@@ -47,6 +53,7 @@ class Controller:
                 pointer-=2
                 pygame.draw.rect(screen, "black", [coord[pointer][0],coord[pointer][1], 45, 45])
               pygame.draw.rect(screen, "black", [coord[pointer][0],coord[pointer][1], 45, 45])
+              guess.pop()
           if pointer==end_limit:
             pass
           else:
@@ -54,106 +61,132 @@ class Controller:
               text_surface = font.render('A', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('a')
             if event.key == pygame.K_b:
               text_surface = font.render('B', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('b')
             if event.key == pygame.K_c:
               text_surface = font.render('C', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('c')
             if event.key == pygame.K_d:
               text_surface = font.render('D', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('d')
             if event.key == pygame.K_e:
               text_surface = font.render('E', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('e')
             if event.key == pygame.K_f:
               text_surface = font.render('F', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('f')
             if event.key == pygame.K_g:
               text_surface = font.render('G', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('g')
             if event.key == pygame.K_h:
               text_surface = font.render('H', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('h')
             if event.key == pygame.K_i:
               text_surface = font.render('I', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('i')
             if event.key == pygame.K_j:
               text_surface = font.render('J', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('j')
             if event.key == pygame.K_k:
               text_surface = font.render('K', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('k')
             if event.key == pygame.K_l:
               text_surface = font.render('L', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('l')
             if event.key == pygame.K_m:
               text_surface = font.render('M', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('m')
             if event.key == pygame.K_n:
               text_surface = font.render('N', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('n')
             if event.key == pygame.K_o:
               text_surface = font.render('O', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('o')
             if event.key == pygame.K_p:
               text_surface = font.render('P', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('p')
             if event.key == pygame.K_q:
               text_surface = font.render('Q', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('q')
             if event.key == pygame.K_r:
               text_surface = font.render('R', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('r')
             if event.key == pygame.K_s:
               text_surface = font.render('S', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('s')
             if event.key == pygame.K_t:
               text_surface = font.render('T', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('t')
             if event.key == pygame.K_u:
               text_surface = font.render('U', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('u')
             if event.key == pygame.K_v:
               text_surface = font.render('V', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('v')
             if event.key == pygame.K_w:
               text_surface = font.render('W', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('w')
             if event.key == pygame.K_x:
               text_surface = font.render('X', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('x')
             if event.key == pygame.K_y:
               text_surface = font.render('Y', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1 
+              guess.append('y')
             if event.key == pygame.K_z:
               text_surface = font.render('Z', False, 'white')
               screen.blit(text_surface, coord[pointer])
               pointer+=1
+              guess.append('z')
             
       
 
@@ -163,6 +196,26 @@ class Controller:
 
     pygame.quit()
     
+  def wordle():
+    return list(random.choice(dictionary.words))
+  
+  def check_pos(wordle, trial):
+    for x, y in zip(wordle, trial):
+      if x == y:
+        Controller.color("correct", x)      #This is when the letter is in the correct position 
+      elif y in wordle:
+        Controller.color("kinda", y)      #This is when the letter is in the wrong position but is still in the word to guess
+      else:
+        Controller.color("wrong", y)    #This is when the letter is incorrect/not in the word that you are trying to guess
+    
+  def color(self, letter):
+    if self == "correct":
+      return letter, "green"  #This part may be wrong, we'll have to test it but the functions should each return the letter and the box color
+    elif self == "kinda":
+      return letter, "yellow"
+    elif self == "wrong":
+      return letter, "black"
+  
   def mainloop(self):
     #wordle = check.Check.wordle()
     #print(wordle)
